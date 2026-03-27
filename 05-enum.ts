c34f951e1b7653beceb327ddacf146d857f84d21
ker-mari <magmayoker@gmail.com>
2026-03-27 16:42:12 +0800

    Done playing and updating the 05-enums.ts

--- 05-enum.ts
+++ 05-enum.ts
@@ -1,14 +1,16 @@
 (() => {
-    enum Role {
+    const Role = {
         // SuperAdmin,
         // SuperAdmin = 1,
-        SuperAdmin = 'SuperAdmin',
+        SuperAdmin : 'SuperAdmin',
         // Teacher,
-        Teacher = 'Teacher',
+        Teacher : 'Sir. Jr',
         // Student,
-        Student = 'Student',
-    }
+        Student : 'Ker',
+    } as const ;
+// equals sign changed to colon :
 
-    // let userRole: Role = 0;
-    let userRole: Role = Role.SuperAdmin;
-})
+    // let userRole: Role = 1;
+    // let userRole: Role = Role.SuperAdmin;
+    console.log(Role.Student);
+})();
